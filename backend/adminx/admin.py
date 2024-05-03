895f24262bd09715f3e1c69891dd85ccd995f58e
HurasAdam <huradam@wp.pl>
2024-05-03 21:14:21 +0200

    Refactor: Remove backend folder to separate frontend and backend into separate repositories.

--- backend/adminx/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from adminx.models import ChangeLog, ContactForm
-
-
-admin.site.register(ChangeLog)
-admin.site.register(ContactForm)
--- backend/tags/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from .models import CustomTags
-
-
-class CustomTagSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomTags
-        fields = "id", "name"
